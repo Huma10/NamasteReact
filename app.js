b330f587b801143a1fdc42c9455890b90cb2215b
Huma10 <khanhuma1021994@gmail.com>
2024-03-31 11:57:09 +0530

    --created index file

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", { id:"heading", xyz:"abc"}, "Hello World From React!")
+console.log('heading', heading);
+ root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading);
